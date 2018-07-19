55c0c6ae48772926d481f40b102322667d5d9db9
maccm <chengming08@163.com>
2018-07-19 20:51:36 +0800

    modify
    完善代码

---
+++ web_backend/models/Afstate.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Afstate = sequelize.define("Afstate", {
+    AFNo:DataTypes.STRING,
+    step:DataTypes.STRING,
+    state:DataTypes.INTEGER,
+    lcNo:DataTypes.STRING,
+    depositAmount:DataTypes.STRING,
+    suggestion:DataTypes.STRING,
+    isAgreed:DataTypes.STRING,
+    backup:DataTypes.STRING,
+});
+
+ 
+  return Afstate;
+};

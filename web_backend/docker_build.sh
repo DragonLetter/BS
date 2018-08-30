90a0601ac8d9504e1d046fcc3652e77b295bfb26
mysear <songsf224@sina.com>
2018-08-30 16:15:35 +0800

    1.拆分镜像build脚本和启动脚本 2.增加挂载容器中日志路径到宿主机路径

--- web_backend/docker_build.sh
+++ web_backend/docker_build.sh
@@ -1,3 +1,10 @@
-cp docker_index.js ./index.js
+#获取当前路径并设置工作路径
+WORKDIR=$(cd $(dirname $0); pwd)
+echo "workdir:"$WORKDIR
+
+#构建镜像前准备工作
+cp $WORKDIR/docker_index.js $WORKDIR/index.js
+
+#删除以前旧的镜像并构建新镜像
+docker rmi web_backend
 docker build -t web_backend .
-docker run -d -p 8080:8080 --name "web_backend" web_backend
---
+++ web_backend/start.sh
@@ -0,0 +1,5 @@
+#创建本地文件夹存储容器中应用产生的日志文件
+mkdir -p ~/web_backend/logs
+
+#启动容器并挂载本地文件到容器中
+docker run --name web_backend_i -p 8080:8080 -v /home/admin/web_backend/logs:/usr/src/node/logs -d web_backend
